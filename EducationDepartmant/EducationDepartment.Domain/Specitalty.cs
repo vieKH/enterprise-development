f9476e6fa8e886ca80741793224e5a2c15f1266c
KH <10122001kh@mail.ru>
2024-09-30 20:07:32 +0400

    Repair Class : Remove UniversityySpecialty, Add property NumberOfGroups in class Specialty

--- EducationDepartmant/EducationDepartment.Domain/Specitalty.cs
+++ EducationDepartmant/EducationDepartment.Domain/Specitalty.cs
@@ -1,8 +1,8 @@
 ﻿namespace EducationDepartment.Domain;
 
-public class Specitalty(string SpecialtyID, string NameSp)
+public class Specitalty(string SpecialtyID, string NameSp, int NumberOfGroups)
 {
     public string SpecialtyID { get; set; } = SpecialtyID;
     public string NameSp { get; set; } = NameSp;
-
+    public int NumberOfGroups { get; set; } = NumberOfGroups; 
 }
--- EducationDepartmant/EducationDepartment.Domain/UniversitySpecialty.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EducationDepartment.Domain;
-
-public class UniversitySpecialty(string RegistrationNumber, string SpecialtyID, int NumberOfGroups)
-{
-    public string RegistrationNumber { get; set;} = RegistrationNumber;
-    public string SpecialtyID { get; set;} = SpecialtyID;
-    public int NumberOfGroups { get; set;} = NumberOfGroups;
-}
